e99534b6647f17e4a7919b42a46e09aa6e561dc7
G100 <g100my.lan229@gmail.com>
2023-03-13 16:19:52 +0800

    set up database in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,32 @@
 const koa = require('koa')
+const { Model } = require('objection')
 const app = new koa()
+
+//
+const Local_Config = {
+  client: 'mysql2',
+  connection: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.LOCAL_DATABASE_PASSWORD, // change password: ALTER USER 'root'@'localhost' IDENTIFIED BY 'NewPassword';
+    database: 'koa', // SHOW DATABASES;
+  },
+}
+const RDS_Config = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.RDS_HOST,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DATABASE,
+  },
+}
+
+const isLocal = !!process.env.IS_LOCAL
+const knex = require('knex')(isLocal ? Local_Config : RDS_Config)
+
+Model.knex(knex)
 
 // logger
 
@@ -9,19 +36,11 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${rt}`)
 })
 
-// x-response-time
-
-app.use(async (ctx, next) => {
-  const start = Date.now()
-  await next()
-  const ms = Date.now() - start
-  ctx.set('X-Response-Time', `${ms}ms`)
-})
-
 // response
 
 app.use(async ctx => {
   ctx.body = 'Hello World'
 })
+
 app.listen(3000)
 console.log(3000)
